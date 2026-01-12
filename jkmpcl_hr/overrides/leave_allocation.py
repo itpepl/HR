6804ddae0d5fda68f8ac94d93ff0a4535ec46d2b
Vanparia-Ayush <ayush@sanskartechnolab.com>
2026-01-12 19:04:52 +0530

    Casual Leave Completed and Sick Leave Started

--- jkmpcl_hr/overrides/leave_allocation.py
+++ jkmpcl_hr/overrides/leave_allocation.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe import _
-from frappe.utils import formatdate
-from hrms.hr.doctype.leave_allocation.leave_allocation import LeaveAllocation
+from frappe.utils import formatdate, flt
+from hrms.hr.doctype.leave_allocation.leave_allocation import LeaveAllocation, get_carry_forwarded_leaves
 from hrms.hr.doctype.leave_allocation.leave_allocation import OverlapError
 
 
@@ -33,3 +33,31 @@ def custom_validate_allocation_overlap(self):
 				+ f': <a href="/app/Form/Leave Allocation/{leave_allocation[0][0]}">{leave_allocation[0][0]}</a>',
 				OverlapError,
 			)
+
+
+class CustomLeaveAllocation(LeaveAllocation):
+	@frappe.whitelist()
+	def set_total_leaves_allocated(self):
+		print(f"\n\n  THIS IS overrided \n\n")
+		self.unused_leaves = flt(
+			get_carry_forwarded_leaves(self.employee, self.leave_type, self.from_date, self.carry_forward),
+			self.precision("unused_leaves"),
+		)
+
+		self.total_leaves_allocated = flt(
+			flt(self.unused_leaves) + flt(self.new_leaves_allocated) + flt(self.custom_opening_balance),
+			self.precision("total_leaves_allocated"),
+		)
+
+		self.limit_carry_forward_based_on_max_allowed_leaves()
+
+		if self.carry_forward:
+			self.set_carry_forwarded_leaves_in_previous_allocation()
+
+		if (
+			not self.total_leaves_allocated
+			and not frappe.db.get_value("Leave Type", self.leave_type, "is_earned_leave")
+			and not frappe.db.get_value("Leave Type", self.leave_type, "is_compensatory")
+		):
+			frappe.throw(_("Total leaves allocated is mandatory for Leave Type {0}").format(self.leave_type))
+		
