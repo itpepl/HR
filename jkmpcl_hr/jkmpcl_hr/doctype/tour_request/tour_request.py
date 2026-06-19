a292bae4a120910e9ab35912806c7d7c7a043d67
nandkishor.bhadada <nandkishor.bhadada@promptdairytech.in>
2026-06-19 17:51:45 +0530

    api and nof days changes done

--- jkmpcl_hr/jkmpcl_hr/doctype/tour_request/tour_request.py
+++ jkmpcl_hr/jkmpcl_hr/doctype/tour_request/tour_request.py
@@ -1,7 +1,8 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import date_diff, add_days
+from frappe.utils import date_diff, add_days,formatdate
+from jkmpcl_hr.jkmpcl_hr.doctype.attendance_lock.attendance_lock import AttendanceLock
 
 class TourRequest(Document):
     
@@ -9,25 +10,20 @@ class TourRequest(Document):
         # Mandatory field validation
         if not self.from_date:
             frappe.throw(_("From Date is mandatory"))
-        
+
         if not self.to_date:
             frappe.throw(_("To Date is mandatory"))
-        
+
         if not self.purpose_of_travel:
             frappe.throw(_("Purpose of Travel is mandatory"))
-        
+
         # To Date cannot be earlier than From Date
         if self.to_date < self.from_date:
             frappe.throw(_("To Date cannot be earlier than From Date"))
-        
-        # Travel duration must be exactly 1 day
-        days_diff = date_diff(self.to_date, self.from_date)
-        
-        if days_diff != 1:
-            frappe.throw(
-                _("Travel duration must be exactly 1 day. To Date should be one day after From Date.")
-            )
-        
+
+        # Calculate No. of Days
+        self.no_of_days = date_diff(self.to_date, self.from_date) + 1
+
         # Check overlapping Tour Request
         existing_tour = frappe.db.sql("""
             SELECT name
@@ -44,14 +40,67 @@ class TourRequest(Document):
             self.to_date,
             self.from_date
         ), as_dict=True)
-        
+
         if existing_tour:
             frappe.throw(
                 _("Tour Request already exists for this date range: {0}").format(
                     existing_tour[0].name
                 )
             )
-    
+
+        # Attendance Lock Validation for Travel Request Date
+        if self.travel_request_date:
+            lock_name = AttendanceLock.is_attendance_locked(
+                self.travel_request_date,
+                self.employee
+            )
+
+            if lock_name:
+                month = frappe.db.get_value(
+                    "Attendance Lock",
+                    lock_name,
+                    "month"
+                )
+
+                if not month:
+                    month = formatdate(
+                        self.travel_request_date,
+                        "MMMM yyyy"
+                    )
+
+                frappe.throw(
+                    _("Attendance is locked for {0}. Travel Request Date is not allowed.").format(month),
+                    title=_("Attendance Lock")
+                )
+
+        # Attendance Lock Validation for Tour Period
+        for i in range(date_diff(self.to_date, self.from_date) + 1):
+            att_date = add_days(self.from_date, i)
+
+            lock_name = AttendanceLock.is_attendance_locked(
+                att_date,
+                self.employee
+            )
+
+            if lock_name:
+                month = frappe.db.get_value(
+                    "Attendance Lock",
+                    lock_name,
+                    "month"
+                )
+
+                if not month:
+                    month = formatdate(
+                        att_date,
+                        "MMMM yyyy"
+                    )
+
+                frappe.throw(
+                    _("Attendance is locked for {0}. Tour Request cannot be created or updated for this period.").format(month),
+                    title=_("Attendance Lock")
+                )
+
+                
     def on_submit(self):
         if self.workflow_state != "Approved by HR":
             return
@@ -92,12 +141,16 @@ class TourRequest(Document):
             att.insert(ignore_permissions=True)
             att.submit()
     
+
     def on_cancel(self):
+        self.db_set("workflow_state", "Cancelled")
         total_days = date_diff(self.to_date, self.from_date) + 1
-        
+
+        employee_doc = frappe.get_doc("Employee", self.employee)
+
         for i in range(total_days):
             att_date = add_days(self.from_date, i)
-            
+
             attendance_list = frappe.get_all(
                 "Attendance",
                 filters={
@@ -108,10 +161,61 @@ class TourRequest(Document):
                 },
                 pluck="name"
             )
-            
+
             for att_name in attendance_list:
-                att = frappe.get_doc("Attendance", att_name)
-                att.cancel()
-                att.delete(ignore_permissions=True)
-        
+                old_att = frappe.get_doc("Attendance", att_name)
+
+                company = old_att.company
+                branch = old_att.custom_branch
+                shift = old_att.shift
+
+                # Cancel and delete On Duty attendance
+                old_att.cancel()
+                old_att.delete(ignore_permissions=True)
+
+                # Check if attendance already exists
+                if not frappe.db.exists(
+                    "Attendance",
+                    {
+                        "employee": self.employee,
+                        "attendance_date": att_date
+                    }
+                ):
+                    # Create Absent Attendance
+                    new_att = frappe.new_doc("Attendance")
+                    new_att.employee = self.employee
+                    new_att.employee_name = employee_doc.employee_name
+                    new_att.attendance_date = att_date
+                    new_att.company = company
+                    new_att.custom_branch = branch
+                    new_att.shift = shift
+                    new_att.status = "Absent"
+
+                    # Penalty fields
+                    new_att.custom_is_penalize = 1
+                    new_att.custom_penalty_leave_type = "Leave Without Pay"
+                    new_att.custom_penalty_leave_count = -1
+                    new_att.custom_remark = "Tour Request Cancel"
+
+                    new_att.insert(ignore_permissions=True)
+                    new_att.submit()
+
+                    # Create Leave Ledger Entry
+                    lle = frappe.new_doc("Leave Ledger Entry")
+                    lle.employee = self.employee
+                    lle.employee_name = employee_doc.employee_name
+                    lle.leave_type = "Leave Without Pay"
+                    lle.from_date = att_date
+                    lle.to_date = att_date
+                    lle.company = company
+                    lle.custom_branch = branch
+                    lle.transaction_type = "Leave Application"
+                    lle.leaves = -1
+                    lle.is_leave_without_pay = 1
+                    lle.custom_is_penalty = 1
+                    lle.custom_attendance = new_att.name
+
+                    lle.insert(ignore_permissions=True)
+                    lle.submit()
+
         frappe.db.commit()
